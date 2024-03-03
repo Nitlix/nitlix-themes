c594724e4ced0d3dab79097eaaada81e445fae8a
Nit <66027553+Nitlix@users.noreply.github.com>
2024-03-03 15:59:31 +0000

    [/] Include types in export

--- src/exports.ts
+++ src/exports.ts
@@ -1,4 +1,7 @@
 import themeInjector from "./themeInjector";
 import themeRetriever from "./themeRetriever";
+import type { Config, ThemeInjectorResult, ThemeRetrieverResult } from "./types";
 
-export { themeInjector, themeRetriever };
+
+
+export { themeInjector, themeRetriever, Config, ThemeInjectorResult, ThemeRetrieverResult };
